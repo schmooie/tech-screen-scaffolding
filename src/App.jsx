a39187022d03e5f017dee77417cd8e30fc3493ea
Sam Lau <samuel.kwun.lau@gmail.com>
2024-07-23 15:38:50 -0400

    make jank traffic light

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import './App.css'
+import { useState, useEffect } from 'react';
 
-function App() {
+const LIGHTS = {
+  GREEN: { duration: 3000, next: 'YELLOW', color: 'green' },
+  YELLOW: { duration: 1000, next: 'RED', color: 'yellow' },
+  RED: { duration: 4000, next: 'GREEN', color: 'red' },
+};
+
+export default function TrafficLight() {
+  const colorLights = Object.keys(LIGHTS);
+  const [currentLight, setCurrentLight] = useState(colorLights[0]);
+
+  useEffect(() => {
+    let timeoutId;
+
+    function cycleLight() {
+      const { duration, next } = LIGHTS[currentLight];
+
+      timeoutId = setTimeout(() => {
+        setCurrentLight(next);
+        cycleLight();
+      }, duration);
+    }
+
+    cycleLight();
+
+    return () => clearTimeout(timeoutId);
+  })
+
   return (
-    <>
-      <h1>Hello, world</h1>
-    </>
+    <section>
+      {colorLights.map(color => LIGHTS[color]).map(({ color, next }) => (
+        <div key={color} className={`${next === LIGHTS[currentLight].next ? 'light--active' : ''} light`} style={{ backgroundColor: color }}></div>
+      ))}
+    </section>
   )
 }
-
-export default App
