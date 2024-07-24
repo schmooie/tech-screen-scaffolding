ab77132bff53b5fdbb92a669c1e8ec9edc27ede0
Sam Lau <samuel.kwun.lau@gmail.com>
2024-07-23 21:52:56 -0400

    make basic tooltip

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import { useState, useId } from 'react';
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1>Hello, world</h1>
+      <Tooltip content="I'm a tooltip" position="right">
+        <h1>Hello, world</h1>
+      </Tooltip>
     </>
   )
+}
+
+function Tooltip({ children, content, position = 'right' }) {
+  const [isActive, setIsActive] = useState(false);
+
+  function onMouseEnter() {
+    setIsActive(true)
+  }
+
+  function onMouseLeave() {
+    setIsActive(false);
+  }
+
+  const id = useId();
+
+  return (<>
+    <div
+      className='relative'
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <div aria-describedby={id}>{children}</div>
+      <div className={`tooltip tooltip--${position} absolute border-solid border-2 rounded-lg p-2 ${!isActive ? 'hidden' : ''}`} role="tooltip" id={id}>
+        {content}
+      </div>
+    </div>
+
+  </>);
 }
 
 export default App
